85e55dbd7f6bedac0a0596ee36f26018d71f18da
mamynavalonar-dev <mamynavalonar@gmail.com>
2026-02-16 10:34:04 +0300

    nous avons fini de concevoir le projet mise à jour à l'avenir si necessaire

--- client/src/components/AnimatedSidebar.jsx
+++ client/src/components/AnimatedSidebar.jsx
@@ -2,6 +2,9 @@ import React, { useCallback, useLayoutEffect, useMemo, useRef, useState, useEffe
 import { NavLink, useLocation } from 'react-router-dom';
 import './animatedSidebar.css';
 
+const LOGO_PRIRTEM = "/ui/login/logos/prirtem.png";
+
+
 const MAIN_OFFSET_COLLAPSED = '110px';
 const MAIN_OFFSET_EXPANDED = '290px';
 
@@ -133,12 +136,13 @@ export default React.memo(function AnimatedSidebar({
           transform: 'translateZ(0)'
         }}
       >
-        <div className="asb-logo">
-          <div className="asb-logo-icon">
-            <ion-icon name="infinite-outline"></ion-icon>
+          <div className="asb-logo">
+            <div className="asb-logo-icon">
+              <img className="asb-logo-img" src={LOGO_PRIRTEM} alt="PRIRTEM" />
+            </div>
+            <div className="asb-logo-text">PRIRTEM</div>
           </div>
-          <div className="asb-logo-text">PRIRTEM</div>
-        </div>
+
 
         {!isMobile && (
           <div className="asb-toggle" onClick={toggle} role="button" aria-label="Réduire/Étendre">
@@ -179,23 +183,28 @@ export default React.memo(function AnimatedSidebar({
 
         
         {/* Footer / Logout */}
-        <div className="asb-footer">
-          <div className="asb-item">
-            <button
-              type="button"
-              className="asb-link asb-logout"
-              onClick={() => {
-                onLogout();
-                if (isMobile) closeMobile();
-              }}
-            >
-              <span className="asb-icon">
-                <ion-icon name="log-out-outline"></ion-icon>
-              </span>
-              <span className="asb-text">Déconnexion</span>
-            </button>
-          </div>
-        </div>
+<div className="asb-footer">
+  <div className="asb-item">
+    <button
+      type="button"
+      className="asb-logoutBtn"
+      onClick={() => {
+        onLogout();
+        if (isMobile) closeMobile();
+      }}
+      aria-label="Déconnexion"
+      title="Déconnexion"
+    >
+      <span className="asb-logoutSign" aria-hidden="true">
+        <svg viewBox="0 0 512 512">
+          <path d="M377.9 105.9L500.7 228.7c7.2 7.2 11.3 17.1 11.3 27.3s-4.1 20.1-11.3 27.3L377.9 406.1c-6.4 6.4-15 9.9-24 9.9c-18.7 0-33.9-15.2-33.9-33.9l0-62.1-128 0c-17.7 0-32-14.3-32-32l0-64c0-17.7 14.3-32 32-32l128 0 0-62.1c0-18.7 15.2-33.9 33.9-33.9c9 0 17.6 3.6 24 9.9zM160 96L96 96c-17.7 0-32 14.3-32 32l0 256c0 17.7 14.3 32 32 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l-64 0c-53 0-96-43-96-96L0 128C0 75 43 32 96 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32z" />
+        </svg>
+      </span>
+      <span className="asb-logoutText">Déconnexion</span>
+    </button>
+  </div>
+</div>
+
       </aside>
     </>
   );
